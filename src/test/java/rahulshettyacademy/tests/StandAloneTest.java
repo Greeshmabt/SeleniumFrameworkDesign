92aea5cfe09706967ff673bcdf1d5398b1b66529
Greeshma BT Gowda <greeshma2998@gmail.com>
2026-04-14 19:08:45 +0400

    Refactor assertions and comments in StandAloneTest

--- src/test/java/rahulshettyacademy/tests/StandAloneTest.java
+++ src/test/java/rahulshettyacademy/tests/StandAloneTest.java
@@ -22,19 +22,19 @@ public class StandAloneTest extends BaseTest{
 		public void submitOrder(String email, String password, String productName, String countryName ) throws IOException { //catching the email,pwd,productName here		
 		//String countryName= "india, UAE";
 		lp.LoginApplication(email,password);
-		ProductCatalog productcat = new ProductCatalog(driver);// calling that method using object
-		List<WebElement>products = productcat.Productslist();// storing in webelements again
+		ProductCatalog productcat = new ProductCatalog(driver);        // calling that method using object
+		List<WebElement>products = productcat.Productslist();// storing in webelements
         //productcat.getProductByName(productName);
 		productcat.addProductToCart(productName);
-		productcat.goToCartPage(); //inheriting from abstract class using child class object coz its linked.
+		productcat.goToCartPage();        //inheriting from abstract class using child class object as its linked.
 	    /* List<WebElement> cartitems= driver.findElements(By.cssSelector(".cartSection h3"));
 	     WebElement b= cartitems.stream().filter(s->s.findElement(By.xpath("//*[text()='ZARA COAT 3']")).getText().equals(productName))
 	     .findFirst().orElse(prod);
 	     Assert.assertTrue(true);*/
 		cartPage cartpage= new cartPage(driver);	
 	Boolean match = cartpage.cartProductMatch(productName);
-	Assert.assertTrue(match); //giving assertTrue here because i know match be True so assertion will pass
-	//driver.findElement(By.cssSelector(".totalRow button")).click();//checkout button
+	Assert.assertTrue(match); //giving assertTrue here as match be True so assertion will pass
+	//driver.findElement(By.cssSelector(".totalRow button")).click();   //checkout button
 	cartpage.cartProducts();
 	cartpage.cartProductMatch(productName);
 	Assert.assertTrue(match);
@@ -42,8 +42,8 @@ public class StandAloneTest extends BaseTest{
 	checkout.checkout();
 	checkout.selectCountry(countryName);
 	checkout.submit();
-	thankyouPage thanks= new thankyouPage(driver);  //this is how u call ur other classess into main class
-	String thank= thanks.thankyou();  //this is how u call ur other classess into main class
+	thankyouPage thanks= new thankyouPage(driver);  //calling other classess into main class
+	String thank= thanks.thankyou();     
 	Assert.assertTrue(thank.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
 	
 	}
@@ -55,7 +55,7 @@ public class StandAloneTest extends BaseTest{
 		OrderPage op= new OrderPage(driver);
 		op.goToOrderHistoryPage();
 		Boolean match= op.verifyOrderDisplay(productName);
-		Assert.assertTrue(match); //if order which u placed is in History , assertion passes
+		Assert.assertTrue(match); //if order placed is in History , assertion passes
 	}
 	@DataProvider
 	public Object[][] getData() {
